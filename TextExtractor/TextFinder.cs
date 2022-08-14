72af8de6d1ac623b954dacb3bbdc53bdf250f510
Alex Sbyshko <alexsbyshko@gmail.com>
2022-08-14 14:16:12 +0700

    Add TextExtractor

---
+++ TextExtractor/TextFinder.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+namespace TextExtractor;
+
+public static class TextFinder
+{
+    private static byte[] Start = {0x00, 0x00, 0x04};
+
+    private const byte ContinueFlag = 0x04;
+    private const byte StopFlag = 0x00;
+
+    public static List<string> FindText(string filePath, out long startTextOffset, out long remainingBytesOffset)
+    {
+        var data = File.ReadAllBytes(filePath);
+        List<string> result = new();
+
+        startTextOffset = FindSequence(data, Start);
+        
+        if (startTextOffset == -1)
+        {
+            remainingBytesOffset = -1;
+            return result;
+        }
+        
+        startTextOffset += Start.Length;
+
+        using var reader = new BinaryReader(new MemoryStream(data));
+        reader.BaseStream.Seek(startTextOffset, SeekOrigin.Begin);
+
+        byte flag;
+        do
+        {
+            var size = reader.ReadInt32();
+            var content =  reader.ReadBytes(size);
+            flag = reader.ReadByte();
+
+            var textString = Encoding.UTF8.GetString(content[..^1]);
+            result.Add(textString);
+        }
+        while (flag == ContinueFlag);
+
+        remainingBytesOffset = reader.BaseStream.Position;
+
+        return result;
+    }
+    
+    private static int FindSequence(byte[] source, byte[] seq)
+    {
+        var start = -1;
+        for (var i = 0; i < source.Length - seq.Length + 1 && start == -1; i++)
+        {
+            var j = 0;
+            for (; j < seq.Length && source[i+j] == seq[j]; j++) {}
+            if (j == seq.Length) start = i;
+        }
+        return start;
+    }
+}
---
+++ TextExtractor/TextReplacer.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+namespace TextExtractor;
+
+public static class TextReplacer
+{
+    private const byte ContinueFlag = 0x04;
+    private const byte StopFlag = 0x00;
+    
+    public static void Replace(string filePath, List<string> newStrings)
+    {
+        var data = File.ReadAllBytes(filePath);
+        var currentText = TextFinder.FindText(filePath, out var startTextOffset, out var remainingBytesOffset);
+        if (startTextOffset == -1)
+        {
+            return;
+        }
+
+        if (currentText.Count != newStrings.Count)
+        {
+            Console.WriteLine($"[ERR] Original file {Path.GetFileName(filePath)} has {currentText.Count} string," +
+                              $" but the new text has {newStrings.Count} strings.");
+        }
+
+        using var reader = new BinaryReader(new MemoryStream(data));
+        
+        reader.BaseStream.Seek(0, SeekOrigin.Begin);
+        var startBytes = reader.ReadBytes((int)startTextOffset);
+        
+        reader.BaseStream.Seek(remainingBytesOffset, SeekOrigin.Begin);
+        var endBytes = reader.ReadBytes(data.Length - (int)remainingBytesOffset);
+
+        using var writer = new BinaryWriter(File.Open(filePath, FileMode.Truncate, FileAccess.Write));
+
+        writer.Write(startBytes);
+
+        for (var i = 0; i < newStrings.Count; i++)
+        {
+            var str = newStrings[i];
+            var bytes = Encoding.UTF8.GetBytes(str);
+            
+            writer.Write(bytes.Length + 1);
+            writer.Write(bytes);
+            writer.Write((byte)0x00);
+
+            if (i < newStrings.Count - 1)
+            {
+                writer.Write(ContinueFlag);
+            }
+            else
+            {
+                writer.Write(StopFlag);
+            }
+        }
+        
+        writer.Write(endBytes);
+    }
+}
